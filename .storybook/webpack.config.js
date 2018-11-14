e139fd644cf3ee7ff18c0f917157d32697790cbe
psychobolt <psychobolt@gmail.com>
2018-11-14 13:54:25 -0800

    Upgrade packages

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,18 +3,10 @@ module.exports = (baseConfig, env, defaultConfig) => ({
   module: {
     ...defaultConfig.module,
     rules: [
-      // Temp fix for issue: https://github.com/storybooks/storybook/issues/4066 and https://github.com/storybooks/storybook/issues/3346
+      // Temp fix for issue: https://github.com/storybooks/storybook/issues/3346
       ...defaultConfig.module.rules.filter(rule => !(
-        (rule.loader && rule.loader.includes(require.resolve('svg-url-loader')))
-        || (rule.use && rule.use.length && rule.use.find(({ loader }) => loader === 'babel-loader'))
+        (rule.use && rule.use.length && rule.use.find(({ loader }) => loader === 'babel-loader'))
       )),
-      {
-        test: /\.svg/,
-        loader: require.resolve('file-loader'),
-        query: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
       {
         test: /\.jsx?$/,
         include: require('path').resolve('./'), // eslint-disable-line global-require
