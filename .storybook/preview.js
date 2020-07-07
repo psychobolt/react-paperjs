60253760382e7967bd722c37fbcd51e88be5485b
psychobolt <560721+psychobolt@users.noreply.github.com>
2020-07-06 22:58:44 -0700

    Register storybook readme decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '@storybook/addon-console';
-import { addParameters } from '@storybook/react';
+import { addParameters, addDecorator } from '@storybook/react';
+import { addReadme } from 'storybook-readme';
 
 // workaround for issue: https://github.com/tuchk4/storybook-readme/issues/221
 addParameters({ options: { theme: {} } });
+
+addDecorator(addReadme);
