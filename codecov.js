1166d4f1a25cdf677f7abbf6b68a84e3a39d96b7
psychobolt <560721+psychobolt@users.noreply.github.com>
2020-07-23 10:15:58 -0700

    Add codecov script

---
+++ codecov.js
@@ -0,0 +1,10 @@
+import spawn from 'cross-spawn';
+
+import projectList from './project-list';
+
+const spawnOptions = { stdio: 'inherit' };
+
+projectList.forEach(({ name, location }) => {
+  spawn.sync('yarn', ['test', '--coverage', '--projects', location], spawnOptions);
+  spawn.sync('codecov', ['--flags', name], spawnOptions);
+});
