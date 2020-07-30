54a6f4dc44a7b35effbe74f2232598b09d22e2bd
psychobolt <560721+psychobolt@users.noreply.github.com>
2020-07-30 11:02:12 -0700

    Fix typo in rollup config

--- rollup.config.common.js
+++ rollup.config.common.js
@@ -16,7 +16,7 @@ const config = {
       include: /node_modules/,
     }),
     babel({
-      include: /node_modules/,
+      exclude: /node_modules/,
       babelHelpers: 'bundled',
     }),
   ],
