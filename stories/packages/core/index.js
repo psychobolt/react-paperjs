2268fbb03b0641e0b6d231e9911c58a2d1b7a654
psychobolt <560721+psychobolt@users.noreply.github.com>
2020-09-02 08:59:08 -0700

    Upgrade packages

--- stories/packages/core/index.js
+++ stories/packages/core/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HelloWorld } from 'react-rollup-boilerplate';
 
-import App from 'stories/App';
+import App from 'stories/shared/App';
 
 export default {
   title: 'packages/core',
